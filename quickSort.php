fab7cd57903eba5fcbe8b230858324a4577b5861
supengfei <tianqibucuo9189@163.com>
2020-05-25 11:18:04 +0800

    qucikSort

---
+++ quickSort.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Class quickSort
+ */
+class quickSort
+{
+    /**
+     * Notes: This is quickSort function
+     * @author supengfei <tianqibucuo9189@163.com>
+     * @link https://github.com/supengfei0625/quick-sort
+     * Date: 2020/5/25
+     * Time: 11:11
+     * @param $arr
+     * @return mixed
+     */
+    public function testSort($arr)
+    {
+        $arrLen = count($arr);
+
+        if($arrLen <= 1){
+            return $arr;
+        }
+        
+    }
+}
